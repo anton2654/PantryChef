a2bad172eba42f9f2d549144b5b036672c24ec79
Галух Антон <galuhanton2654@gmail.com>
2026-03-12 16:21:19 +0200

    Clean: removed tracking for wwwroot/lib and updated gitignore

---
+++ сума цифр числа/сума цифр числа/сума цифр числа.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+
+	string txt = to_string(number);
+
+	int sum = 0;
+	for (int i = 0; i < txt.length(); i++) sum += (int)(number / pow(10, i)) % 10;
+
+	cout << sum;
+
+	return 0;
+}
